8c21e66b76ebf33da41c155620f913467fae1f13
learnuidev <learnuidev@gmail.com>
2022-04-15 21:18:28 -0400

    init

---
+++ bin/clojure_dart_example.dart
@@ -0,0 +1,1 @@
+export "../lib/cljd-out/quickstart/helloworld.dart" show main;
---
+++ lib/cljd-out/quickstart/helloworld.dart
@@ -0,0 +1,10 @@
+import "dart:core" as dc;
+import "helloworld.dart" as Uvpclcoq_helloworld;
+import "../cljd/core.dart" as Uvpclcoc_core;
+
+// BEGIN main
+dc.dynamic main(){
+return (Uvpclcoc_core.print.$_invoke$1("hello, world\n", ));
+}
+
+// END main
